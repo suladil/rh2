039c39889933fa07b290a87575933d6e7fa878c3
KP Parrish <krparrish@adobe.com>
2024-06-12 10:35:29 -0600

    lookbook teasers rendering

--- blocks/image-cta/image-cta.js
+++ blocks/image-cta/image-cta.js
@@ -1,4 +1,3 @@
 export default function decorate(block) {
-	const cols = [...block.firstElementChild.children];
-	console.log(cols)
+
   }
--- blocks/image-list/image-list.js
+++ blocks/image-list/image-list.js
@@ -1,7 +1,6 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
-
   async function fetchJson(link) {
     const response = await fetch(link?.href);
 
@@ -14,13 +13,42 @@ export default async function decorate(block) {
   }
 
   const blockLink = block.querySelector('a');
+  const blockLinkWrapper = blockLink.closest('div').parentElement;
+  console.log(blockLinkWrapper);
+
+  const isLookbook = block.classList.contains('lookbook-teasers');
+
   const listData = await fetchJson(blockLink);
-  listData.forEach((image, idx) => {
-	const img = createOptimizedPicture(image.image, image.title, false, [{ width: 2500 }]);
+  listData.forEach(image => {
+	var img = '';
+	if(!isLookbook){
+		img = createOptimizedPicture(image.image, image.title, false, [{ width: 2500 }]);
+	} else {
+		img = createOptimizedPicture(image.teaserImg, image.bookTitle, false, [{ width: 2500 }]);
+	}
 	img.lastElementChild.width = '1200';
 	img.lastElementChild.height = '1000';
-	
+
 	const createdSection = document.createElement('div');
+
+	if(isLookbook){
+		if(image.featuredTeaser === 'true'){		
+			createdSection.innerHTML = `
+			<div class="image-list-item">
+				<a href=${image.wrappingLink} target="_blank">
+					${img.outerHTML}
+					<div class="teaser-content">
+						<img src=${image.teaserHeading} />
+						<div class="buttons-container">
+							<a href=${image.teaserLink1}>Explore the sourcebook</a>
+							<a href=${image.teaserLink2}>shop the collections</a>
+						</div>
+					</div>
+				</a>
+			</div>
+		`;	
+	  }
+	} else {
 	createdSection.innerHTML = `
 	  <div class="image-list-item">
 	 	<a href=${image.link} target="_blank">
@@ -28,6 +56,7 @@ export default async function decorate(block) {
 		</a>
 	  </div>
 	`;
+	}
 
 	block.append(createdSection);
   })
